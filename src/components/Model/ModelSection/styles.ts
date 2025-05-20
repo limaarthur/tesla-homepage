0a68e21481c0e45d4d3d688f5abee08846ac00c5
limaarthur <artglima.dev@gmail.com>
2025-05-20 18:11:41 -0300

    feat: setting the scroll of elements with scroll snap

--- src/components/Model/ModelSection/styles.ts
+++ src/components/Model/ModelSection/styles.ts
@@ -2,4 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   height: 100vh; // Cada carro irá ocupar 100% de altura da tela
+
+  scroll-snap-align: start; // Define o alinhamento do scroll para o elemento pai
 `;
--- src/components/Model/ModelsWrapper/styles.ts
+++ src/components/Model/ModelsWrapper/styles.ts
@@ -1,3 +1,8 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh; // Altura de 100% para o componente pai
+
+  scroll-snap-type: y mandatory; // Eixo y pois iremos fazer o efeito scroll de cima para baixo
+  overflow-y: scroll;
+`;
