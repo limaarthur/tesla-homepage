76014fa7dba65dfcf6c97325c2f11396ea628af5
limaarthur <artglima.dev@gmail.com>
2025-05-29 11:48:44 -0300

    feat: applying opacity footer

--- src/components/UniqueOverlay/index.tsx
+++ src/components/UniqueOverlay/index.tsx
@@ -1,16 +1,22 @@
+import { useWrapperScroll } from '../Model/useWrapperScroll';
+import { useTransform } from 'framer-motion';
+
 import logoImg from '../../../public/images/logo.svg'
 import hamburguerMenu from '../../../public/images/hamburger-menu.svg'
 
-import { Container, Header, Footer } from './styles'
+import { Container, Header, Logo, Menu, Footer } from './styles'
 
 export function UniqueOverlay() {
+  const { scrollYProgress } = useWrapperScroll() 
+
+  const opacity = useTransform(scrollYProgress, [0.9, 1], [0, 1]) // Opacidade ao fazer o scroll 0.9 = 90% e 1 = 100%
   return (
     <Container>
       <Header>
-        <img src={logoImg} alt="Tesla" />
-        <img src={hamburguerMenu} alt="Menu" />
+        <Logo src={logoImg} alt="Tesla" />
+        <Menu src={hamburguerMenu} alt="Menu" />
       </Header>
-      <Footer>
+      <Footer style={{ opacity }}>
         <ul>
           <li>
             <a href="#">Tesla</a>
--- src/components/UniqueOverlay/styles.ts
+++ src/components/UniqueOverlay/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 export const Container = styled.div`
   position: sticky; // Permite fazer o scroll pois o fixed não permite isso
@@ -8,20 +9,76 @@ export const Container = styled.div`
   bottom: 0;
 `;
 
-export const Header = styled.div`
+export const Header = styled.header`
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
 
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0 1.2rem;
+  min-height: 3.2rem;
 `;
 
-export const Logo = styled.div``;
+export const Logo = styled.img`
+  height: 1.2rem;
+  cursor: pointer;
+`;
 
-export const Burger = styled.div``;
+export const Menu = styled.img`
+  width: 1.7rem;
+  height: 1.7rem;
+  cursor: pointer;
 
-export const Footer = styled.div``;
+  padding-right: 0.3rem;
+`;
 
-// parei em 1:20:00
+export const Footer = styled(motion.footer)`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  ul { // Mobile first ao diminuir a tela ficará em coluna
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    li {
+      list-style: none;
+      font-size: 1rem;
+
+      & + li {
+        margin-top: 0.6rem 0 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: #393c41;
+
+        &:hover {
+          color: #000000;
+        }
+      }
+    }
+  }
+
+  margin-bottom: 1.8rem;
+
+  @media (min-width: 37.5rem) { // Em telas com 600px acima o margin será aplicado
+    margin-bottom: 2.3rem;
+  }
+
+  ul {
+    flex-direction: row;
+
+    li + li {
+    margin: 0 0 0 1.8rem;
+    }
+  }
+`;
 
